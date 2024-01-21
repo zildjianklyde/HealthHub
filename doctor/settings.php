22e5b756f07272173f9185cecf4c064d68b25d48
zildjianklyde <zildjianklyde@gmail.com>
2024-01-21 18:28:22 +0800

    asd

--- doctor/settings.php
+++ doctor/settings.php
@@ -4,13 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/login.css">  
-    <link rel="stylesheet" href="./css/signup.css">  
-    <link rel="stylesheet" href="./css/main.css">  
-    <link rel="stylesheet" href="./css/index.css">
-    <link rel="stylesheet" href="./css/patient.css">
-    <link rel="stylesheet" href="./css/admin.css">
-    <link rel="stylesheet" href="./css/front-inter.css">
+    <link rel="stylesheet" href="../css/main.css">  
+    <link rel="stylesheet" href="../css/admin.css">
         
 
 
